2b28e5fb916dbd3b913e22d4b25035d984a0a225
VladMihalache <59883066+VladMihalache@users.noreply.github.com>
2020-07-22 15:08:59 +0300

    Added "Aplicatii" section + little change
    Few of the changes were adding The fonts, comments in styling pages and  cleaning the code.

--- src/components/Aplicatii.js
+++ src/components/Aplicatii.js
@@ -11,6 +11,58 @@ export class Aplicatii extends Component {
                 <div className={style.exerciseSection}>
                     <div className={style.exercise}>
                         <div className={style.exerciseTask}>
+                            <h4><span>1.</span> Alcătuiește planul dezvoltat de idei, adăugând detalii pentru fiecare idee principală din planul simplu.</h4>
+                        </div>
+                        <div className={style.exerciseBodyOne}>
+                            <div className={style.pair}>
+                                <div className={`${style.leftSide} ${style.firstPair}`}>
+                                    <div className={style.topLeft}>
+                                        <h4>Planul simplu de idei</h4>
+                                    </div>
+                                    <div className={style.mainBoxes}>
+                                        <h4 className={style.orangeBox}>Situația inițială</h4>
+                                        <p className={style.blueBox}><span>Un domn vine în vizită la doamna Popescu cu ocazia onomasticii fiului ei.</span></p>
+                                    </div>
+                                </div>
+                                <div className={`${style.rightSide} ${style.firstPair}`}>
+                                    <div className={style.topRight}>
+                                        <h4>Planul dezvoltat de idei</h4>
+                                    </div>
+                                    <div className={style.mainBoxes}>
+                                        <p className={style.blueBox}><span>Un domn vine în vizită la doamna Popescu cu ocazia onomasticii fiului ei.</span> Musafirul îi oferă lui Ionel o minge și începe să discute cu gazda.</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className={style.pair}>
+                                <div className={style.leftSide}>
+                                    <div className={style.mainBoxes}>
+                                        <h4 className={style.orangeBox}>Succesiunea de întâmplări</h4>
+                                        <textarea className={style.blueBox}></textarea>
+                                    </div>
+                                </div>
+                                <div className={style.rightSide}>
+                                    <div className={style.mainBoxes}>
+                                        <textarea className={style.blueBox}></textarea>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className={style.pair}>
+                                <div className={style.leftSide}>
+                                    <div className={style.mainBoxes}>
+                                        <h4 className={style.orangeBoxLast}>Situația finală</h4>
+                                        <textarea className={style.blueBox}></textarea>
+                                    </div>
+                                </div>
+                                <div className={style.rightSide}>
+                                    <div className={style.mainBoxes}>
+                                        <textarea className={style.blueBox}></textarea>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={style.exercise}>
+                        <div className={style.exerciseTask}>
                             <h4><span>2.</span> Evaluează planul dezvoltat al colegului/colegei de bancă, pe baza următoarelor criterii:</h4>
                         </div>
                         <div className={style.exerciseBodyTwo}>
